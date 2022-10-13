fbd5d820fc0248191e93702f9cc1000eaa02eaf1
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-10-14 02:29:22 +0300

    Navigation nearly polished
    Few bugs:
    1. when opening site while screen is small, side navigation shows or main moves over
    2. when on mobile, hamburger menu stays open when clicked

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,21 @@
 // When website loads navbar script hide one navbar depending of the current window size
 if (window.innerWidth < 576) {
+  $("#sidebar").removeClass("active-nav");
+  $(".my-container").removeClass("active-cont");
   $("#navbar2").show();
   $("#navbar1").hide();
   $("#side-menu-btn").hide();
 } else {
+  //if screen is wide, show the sidebar menu by adding classes
   $("#navbar2").hide();
   $("#navbar1").show();
   $("#side-menu-btn").show();
 }
+
 // When window is being rezised hide one navbar depending of the current window size
 $(window).resize(function () {
   if (window.innerWidth < 576) {
+    // When we want to show smaller navbar we need to remove the class
     if ($(".my-container").hasClass("active-cont")) {
       $(".my-container").toggleClass("active-cont");
     }
@@ -26,9 +31,22 @@ $(window).resize(function () {
     $("#side-menu-btn").show();
   }
 });
+
+// Side menu button functionality
 if ($("#side-menu-btn") != null) {
+  // When button is clicked toggle class names to activate animations from css
   $("#side-menu-btn").on("click", () => {
     $("#sidebar").toggleClass("active-nav");
     $(".my-container").toggleClass("active-cont");
   });
+  $("#side-menu-btn").hover(
+    // When mouse is over
+    function () {
+      $("#side-menu-btn").html(">");
+    },
+    // When mouse leave
+    function () {
+      $("#side-menu-btn").html("");
+    }
+  );
 }
