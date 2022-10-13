ad695d8694ab22241081d45790f6d94e44f3d64f
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-10-13 12:22:18 +0300

    Visual updates
    new fonts and ui changes.
    Also got hamburger navigation to work properly.

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,18 @@
 // When website loads navbar script hide one navbar depending of the current window size
-if(window.innerWidth < 576){
+if (window.innerWidth < 576) {
+  $("#navbar2").show();
+  $("#navbar1").hide();
+} else {
+  $("#navbar2").hide();
+  $("#navbar1").show();
+}
+// When window is being rezised hide one navbar depending of the current window size
+$(window).resize(function () {
+  if (window.innerWidth < 576) {
     $("#navbar2").show();
     $("#navbar1").hide();
-}
-else{
+  } else {
     $("#navbar2").hide();
     $("#navbar1").show();
-}
-
-// When window is being rezised hide one navbar depending of the current window size
-$( window ).resize(function() {
-    if(window.innerWidth < 576){
-        $("#navbar2").show();
-        $("#navbar1").hide();
-    }
-    else{
-        $("#navbar2").hide();
-        $("#navbar1").show();
-    }
-  });
+  }
+});
