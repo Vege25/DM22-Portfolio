dadd5573d277302da030c301fd088c7f255f2951
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-10-13 14:32:05 +0300

    Navigation FINALLY works
    Got navigation to work like i want to.
    Now only need to style them and add content.

--- js/navbar.js
+++ js/navbar.js
@@ -2,17 +2,33 @@
 if (window.innerWidth < 576) {
   $("#navbar2").show();
   $("#navbar1").hide();
+  $("#side-menu-btn").hide();
 } else {
   $("#navbar2").hide();
   $("#navbar1").show();
+  $("#side-menu-btn").show();
 }
 // When window is being rezised hide one navbar depending of the current window size
 $(window).resize(function () {
   if (window.innerWidth < 576) {
+    if ($(".my-container").hasClass("active-cont")) {
+      $(".my-container").toggleClass("active-cont");
+    }
+    if ($("#sidebar").hasClass("active-nav")) {
+      $("#sidebar").toggleClass("active-nav");
+    }
     $("#navbar2").show();
     $("#navbar1").hide();
+    $("#side-menu-btn").hide();
   } else {
     $("#navbar2").hide();
     $("#navbar1").show();
+    $("#side-menu-btn").show();
   }
 });
+if ($("#side-menu-btn") != null) {
+  $("#side-menu-btn").on("click", () => {
+    $("#sidebar").toggleClass("active-nav");
+    $(".my-container").toggleClass("active-cont");
+  });
+}
