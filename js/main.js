c26b60d4c9c100c7733c6d479f2fc5a4b4ba4a9f
VeetiSo <99474309+VeetiSo@users.noreply.github.com>
2022-10-17 17:58:20 +0300

    Big update
    Added every jquery task.
    Navigation funcitonality added.
    Fixed few bugs
    Made responsivity better.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ $(".visit-site-btn").on("click", () => {
   window.location =
     "https://users.metropolia.fi/~veetiso/DM-22/jquery1/jqbasiclab1.html";
 });
+
+$("#query-4").on("click", () => {
+  window.location =
+    "https://users.metropolia.fi/~veetiso/DM-22/jquery4/ex4.html";
+});
+
+$("#query-5").on("click", () => {
+  window.location =
+    "https://users.metropolia.fi/~veetiso/DM-22/jquery5/animate.html";
+});
+
+$("#query-a").on("click", () => {
+  window.location = "https://users.metropolia.fi/~veetiso/DM-22/AJAX/haku.html";
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,76 +1,116 @@
-// When website loads navbar script hide one navbar depending of the current window size
-if (window.innerWidth < 576) {
-  $("#sidebar").removeClass("active-nav");
-  $(".my-container").removeClass("active-cont"); // TODO the bar still shows
-  $("#navbar2").show();
-  $("#navbar1").hide();
-  $("#side-menu-btn").hide();
-} else {
-  //if screen is wide, show the sidebar menu by adding classes
-  $("#navbar2").hide();
-  $("#navbar1").show();
-  $("#side-menu-btn").show();
-}
+//On start
+activeNavbarController(); // THIS FIRST!
+scrollToTask();
+menuButton();
 
-// When window is being rezised hide one navbar depending of the current window size
-$(window).resize(function () {
+function activeNavbarController() {
+  // When website loads navbar script hide one navbar depending of the current window size
   if (window.innerWidth < 576) {
-    // When we want to show smaller navbar we need to remove the class
-    if ($(".my-container").hasClass("active-cont")) {
-      $(".my-container").toggleClass("active-cont");
-    }
-    if ($("#sidebar").hasClass("active-nav")) {
-      $("#sidebar").toggleClass("active-nav");
-    }
+    $("#sidebar").removeClass("active-nav");
+    $(".my-container").removeClass("active-cont"); // TODO the bar still shows
     $("#navbar2").show();
     $("#navbar1").hide();
     $("#side-menu-btn").hide();
   } else {
+    //if screen is wide, show the sidebar menu by adding classes
     $("#navbar2").hide();
     $("#navbar1").show();
     $("#side-menu-btn").show();
   }
-});
 
-// Side menu button functionality
-if ($("#side-menu-btn") != null) {
-  // When button is clicked toggle class names to activate animations from css
-  $("#side-menu-btn").on("click", () => {
-    $("#sidebar").toggleClass("active-nav");
-    $(".my-container").toggleClass("active-cont");
-  });
-  $("#side-menu-btn").hover(
-    // When mouse is over
-    function () {
-      $("#side-menu-btn").html(">");
-    },
-    // When mouse leave
-    function () {
-      $("#side-menu-btn").html("");
+  // When window is being rezised hide one navbar depending of the current window size
+  $(window).resize(function () {
+    if (window.innerWidth < 576) {
+      // When we want to show smaller navbar we need to remove the class
+      if ($(".my-container").hasClass("active-cont")) {
+        $(".my-container").toggleClass("active-cont");
+      }
+      if ($("#sidebar").hasClass("active-nav")) {
+        $("#sidebar").toggleClass("active-nav");
+      }
+      $("#navbar2").show();
+      $("#navbar1").hide();
+      $("#side-menu-btn").hide();
+    } else {
+      $("#navbar2").hide();
+      $("#navbar1").show();
+      $("#side-menu-btn").show();
     }
-  );
+  });
 }
 
-// Task section controller
-$("#n1-task-1").on("click", () => {
-  $("html, body").animate(
-    {
-      scrollTop: $("#section-2-title").offset().top - 25,
-    },
-    50
-  );
-});
-$("#n2-task-1").on("click", () => {
-  toggleNavBar2();
-  $("html, body").animate(
-    {
-      scrollTop: $("#section-2-title").offset().top - 100,
-    },
-    50
-  );
-});
+function scrollToTask() {
+  // Task section controller
+  //First task
+  $("#n1-task-1").on("click", () => {
+    scrollToElement($("#section-2-title"), 25);
+  });
+  $("#n2-task-1").on("click", () => {
+    toggleNavBar2(); // Hide 2nd  navigation when navigation is clicked
+    scrollToElement($("#section-2-title"), 100);
+  });
 
+  //Second task
+  $("#n1-task-2").on("click", () => {
+    scrollToElement($(".query-4"), 40);
+  });
+  $("#n2-task-2").on("click", () => {
+    toggleNavBar2(); // Hide 2nd  navigation when navigation is clicked
+    scrollToElement($(".section3"), 25);
+  });
+
+  //Third task
+  $("#n1-task-3").on("click", () => {
+    scrollToElement($(".query-5"), 40);
+  });
+  $("#n2-task-3").on("click", () => {
+    toggleNavBar2(); // Hide 2nd  navigation when navigation is clicked
+    scrollToElement($(".query-5"), 125);
+  });
+
+  //Fourth task
+  $("#n1-task-4").on("click", () => {
+    scrollToElement($(".query-a"), 40);
+  });
+  $("#n2-task-4").on("click", () => {
+    toggleNavBar2(); // Hide 2nd  navigation when navigation is clicked
+    scrollToElement($(".query-a"), 125);
+  });
+}
+
+// Hide 2nd  navigation when navigation is clicked
 function toggleNavBar2() {
   $("#navbarSupportedContent").toggleClass("show");
   $(".navbar-toggler").toggleClass("collapsed");
+}
+
+// Side menu button functionality
+function menuButton() {
+  if ($("#side-menu-btn") != null) {
+    // When button is clicked toggle class names to activate animations from css
+    $("#side-menu-btn").on("click", () => {
+      $("#sidebar").toggleClass("active-nav");
+      $(".my-container").toggleClass("active-cont");
+    });
+    $("#side-menu-btn").hover(
+      // When mouse is over
+      function () {
+        $("#side-menu-btn").html(">");
+      },
+      // When mouse leave
+      function () {
+        $("#side-menu-btn").html("");
+      }
+    );
+  }
+}
+
+function scrollToElement(target, offset) {
+  console.log(target);
+  $("html, body").animate(
+    {
+      scrollTop: target.offset().top - offset,
+    },
+    50
+  );
 }
